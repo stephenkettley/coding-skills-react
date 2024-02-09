bcbda3d40c1b3b2f5e2a011a58d57579cee5c7e5
Stephen Kettley <kettleystephen@gmail.com>
2024-02-09 16:36:06 +0200

    update responsiveness

--- src/components/SkillGroup.tsx
+++ src/components/SkillGroup.tsx
@@ -25,17 +25,17 @@ type SkillGroupProps = {
 
 const SkillGroup = ({ groupHeading, skills }: SkillGroupProps) => {
   return (
-    <div className="flex flex-col gap-4 items-center justify-center bg-slate-100 shadow-xl shadow-slate-600 max-w-screen w-8/12 py-8 my-8 px-10 rounded-2xl">
+    <div className="flex flex-col gap-4 items-center justify-center bg-slate-100 shadow-xl shadow-slate-600 max-w-screen md:max-w-[850px] w-10/12  py-8 my-8 px-10 rounded-2xl">
       <h1 className="font-bold text-3xl my-8 text-center">{groupHeading}.</h1>
       <Carousel
         opts={{
           align: "end",
         }}
-        className="w-full md:max-w-md lg:max-w-2xl h-full py-2 select-none"
+        className="lg:max-w-2xl sm:max-w-[450px] h-full py-2 select-none"
       >
-        <CarouselContent className="py-5">
+        <CarouselContent className="py-3">
           {skills.map((skill) => (
-            <CarouselItem className="lg:basis-1/2 md:basis-1/2">
+            <CarouselItem className="md:basis-1/2 sm:basis-1/2">
               <div className="flex items-center justify-center">
                 <SkillCard
                   key={skill.id}
